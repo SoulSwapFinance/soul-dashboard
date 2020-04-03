203c6f98591ac5c2a8ffa1162027ba29f386cea0
Pavel Spacil <pavel.spacil@gmail.com>
2020-04-03 16:36:09 +0200

    Add ARIA helper functions and fantom-web3-wallet plugin (WIP)

---
+++ src/plugins/fantom-web3-wallet.js
@@ -0,0 +1,53 @@
+/**
+ * Fantom web3 wallet plugin for VUE, based on https://github.com/Fantom-foundation/fantom-web3-wallet
+ */
+export default {
+    install(_Vue, _options) {
+        const Web3 = _options.Web3;
+        const httpProvider = _options.httpProvider;
+
+        if (!Web3) {
+            throw new Error('Set Web3 library');
+        }
+
+        if (!httpProvider) {
+            throw new Error('Set http provider for Web3');
+        }
+
+        const web3 = new Web3(httpProvider);
+
+        _Vue.prototype.$fWallet = {
+            web3,
+
+            async getBalance(_address) {
+                return await web3.eth.getBalance(_address);
+            },
+
+            restoreAccountByPrivateKey(_privateKey) {
+                return web3.eth.accounts.privateKeyToAccount(_privateKey);
+            },
+
+            encryptToKeystore(_privateKey, _password) {
+                return web3.eth.accounts.encrypt(_privateKey, _password);
+            },
+
+            decryptFromKeystore(_keystoreJsonV3, _password) {
+                return web3.eth.accounts.decrypt(_keystoreJsonV3, _password);
+            },
+
+            isPrivateKey(_key) {
+                let pk = _key;
+
+                if (pk.substring(0, 2) !== '0x') {
+                    pk = `0x${pk}`;
+                }
+
+                if ((pk.length !== 66) || !web3.utils.isHexStrict(pk)) {
+                    pk = '';
+                }
+
+                return pk;
+            }
+        };
+    }
+};
---
+++ src/utils/aria.js
@@ -0,0 +1,145 @@
+const RECEIVE_FOCUS_FROM_ATTR = 'data-receive-focus-from';
+
+/**
+ * Is element visible? Fast method, but not accurate, if fixed element is on page.
+ * (window.getComputedStyle(el) (style.display === 'none') -> much slower).
+ *
+ * @param {HTMLElement} _elem
+ * @return {boolean}
+ */
+export function isVisible(_elem) {
+    return !!_elem && _elem.offsetParent !== null;
+}
+
+/**
+ * @param {HTMLElement} _elem
+ * @return {boolean}
+ */
+export function isFocusable(_elem) {
+    let focusable = false;
+
+    if (!_elem || _elem.disabled) {
+        return false;
+    }
+
+    switch (_elem.nodeName) {
+        case 'INPUT':
+            focusable = _elem.type !== 'hidden' && _elem.type !== 'file';
+            break;
+        case 'A':
+            focusable = !!_elem.href && _elem.rel !== 'ignore';
+            break;
+        case 'BUTTON':
+        case 'SELECT':
+        case 'TEXTAREA':
+            focusable = true;
+            break;
+        default:
+            focusable = false;
+    }
+
+    if (!focusable) {
+        focusable =
+            _elem.tabIndex > 0 || (_elem.tabIndex === 0 && _elem.getAttribute('tabIndex') !== null);
+    }
+
+    if (focusable) {
+        focusable = isVisible(_elem.offsetParent);
+    }
+
+    return focusable;
+}
+
+export function findFirstFocusableDescendant(_elem) {
+    let focusableElem = null;
+    let childNodes;
+    let child;
+
+    if (_elem) {
+        childNodes = _elem.assignedNodes ? _elem.assignedNodes() : _elem.childNodes;
+
+        // Edge/IE
+        if (!childNodes && _elem.assignedNodes) {
+            childNodes = _elem.childNodes;
+        }
+
+        if (childNodes) {
+            for (let i = 0, len1 = childNodes.length; i < len1; i += 1) {
+                child = childNodes[i];
+                if (child.nodeType === 1) {
+                    if (isFocusable(child)) {
+                        focusableElem = child;
+                        break;
+                    } else {
+                        focusableElem = findFirstFocusableDescendant(child);
+                        if (focusableElem) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return focusableElem;
+}
+
+export function findLastFocusableDescendant(_elem) {
+    let focusableElem = null;
+    let childNodes;
+    let child;
+
+    if (_elem) {
+        childNodes = _elem.assignedNodes ? _elem.assignedNodes() : _elem.childNodes;
+
+        // Edge/IE
+        if (!childNodes && _elem.assignedNodes) {
+            childNodes = _elem.childNodes;
+        }
+
+        if (childNodes) {
+            for (let i = childNodes.length - 1; i >= 0; i -= 1) {
+                child = childNodes[i];
+                if (child.nodeType === 1) {
+                    // console.log('la', child.assignedNodes, (child.assignedNodes ? child.assignedNodes() : ''));
+                    if (isFocusable(child)) {
+                        // return child;
+                        focusableElem = child;
+                        break;
+                    } else {
+                        focusableElem = findLastFocusableDescendant(child);
+                        if (focusableElem) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return focusableElem;
+}
+
+/**
+ * @param {string} _id
+ */
+export function returnFocus(_id) {
+    const elem = document.querySelector(`[${RECEIVE_FOCUS_FROM_ATTR}="${_id}"]`);
+
+    if (elem) {
+        elem.removeAttribute(RECEIVE_FOCUS_FROM_ATTR);
+        elem.focus();
+    } else {
+        document.body.focus();
+    }
+}
+
+/**
+ * Set `RECEIVE_FOCUS_FROM_ATTR` attribute to an element with focus.
+ * @param {string} _id
+ */
+export function setReceiveFocusFromAttr(_id) {
+    if (document.activeElement) {
+        document.activeElement.setAttribute(RECEIVE_FOCUS_FROM_ATTR, _id);
+    }
+}
