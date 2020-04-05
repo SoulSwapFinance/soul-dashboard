4ef8f03cd6dd08aac11a230942898f2282e2e81c
Pavel Spacil <pavel.spacil@gmail.com>
2020-04-05 20:36:24 +0200

    Create basic form component and PrivateKeyForm
    Create restore account view (WIP)

---
+++ src/components/forms/PrivateKeyForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="ff">
+        <FForm @f-form-submit="onPrivateKeyFormSubmit">
+            <fieldset class="">
+                <legend>Private key</legend>
+
+                <div class="main">
+                    <label for="pk">Please type in your private key</label>
+                    <input type="text" class="large" id="pk" name="pk" v-model="dPk" @input="onPkInput">
+                </div>
+
+                <div class="footer">
+                    <button type="submit" class="large" :disabled="dDisabled">Unlock account</button>
+                </div>
+            </fieldset>
+        </FForm>
+    </div>
+</template>
+
+<script>
+    import FForm from "../core/FForm/FForm.vue";
+
+    export default {
+        components: {
+            FForm
+        },
+
+        data() {
+            return {
+                dPk: '',
+                dDisabled: true
+            }
+        },
+
+        computed: {
+            cPk() {
+                return this.$fWallet.isPrivateKey(this.dPk.trim());
+            }
+        },
+
+        methods: {
+            onPrivateKeyFormSubmit(_event) {
+                _event.detail.data.pk = this.cPk;
+                this.$emit('f-form-submit', _event)
+            },
+
+            onPkInput() {
+                const pk = this.cPk;
+
+                this.dDisabled = !pk;
+
+                if (!this.dDisabled) {
+                    this.dPk = pk;
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/RestoreAccount/RestoreAccount.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="view-restore-account">
+        <f-card class="window">
+            <div class="header">
+                <h1>Restore account</h1>
+            </div>
+            <div class="body">
+                <h2>Private key</h2>
+
+                <f-private-key-form @f-form-submit="onPrivateKeyFormSubmit" />
+            </div>
+        </f-card>
+        <component v-bind:is="currentTabComponent"></component>
+    </div>
+</template>
+
+<script>
+    import FCard from "../../components/core/FCard/FCard.vue";
+    import FPrivateKeyForm from "../../components/forms/PrivateKeyForm.vue";
+    import {findFirstFocusableDescendant} from "../../utils/aria.js";
+
+    export default {
+        components: {FPrivateKeyForm, FCard},
+
+        mounted() {
+            const el = findFirstFocusableDescendant(this.$el);
+            if (el) {
+                el.focus();
+            }
+            // setTimeout(() => {findFirstFocusableDescendant(this.$el)}, 10);
+            // this.$nextTick(() => {findFirstFocusableDescendant(this.$el)});
+        },
+
+        methods: {
+            onPrivateKeyFormSubmit(_event) {
+                const pk = _event.detail.data.pk;
+
+                if (pk) {
+                    console.log('JO!!', pk);
+                    this.$router.replace({name: 'create-account'});
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "style";
+</style>
