2e0eb805611046d13c021eb74bbbdf44f79af86f
Pavel Spacil <pavel.spacil@gmail.com>
2020-04-30 14:25:53 +0200

    Create window with acount address and qr code

--- src/components/AddressActionsBox/AddressActionsBox.vue
+++ src/components/AddressActionsBox/AddressActionsBox.vue
@@ -5,7 +5,11 @@
                 <f-copy-button :text="currentAccount.address" class="btn large light same-size round" />
             </li>
             <li>
-                <button class="btn large light same-size round" title="qr code of address" @click="onButtonClick">
+                <button
+                    class="btn large light same-size round"
+                    title="qr code of address"
+                    @click="$refs.qrWindow.show()"
+                >
                     <icon data="@/assets/svg/qr.svg" width="20" height="20" aria-hidden="true" />
                 </button>
             </li>
@@ -28,15 +32,29 @@
                 </router-link>
             </li>
         </ul>
+
+        <f-window
+            ref="qrWindow"
+            modal
+            title="Address"
+            style="max-width: 620px;"
+            transition-enter="scale-center-enter-active"
+            transition-leave="scale-center-leave-active"
+        >
+            <h3 class="break-word h2 align-center" :class="addressCssClass">{{ currentAccount.address }}</h3>
+            <vue-q-r-code-component :text="currentAccount.address" class="qr-code" />
+        </f-window>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import FCopyButton from '../core/FCopyButton/FCopyButton.vue';
+import VueQRCodeComponent from 'vue-qrcode-component';
+import FWindow from '../core/FWindow/FWindow.vue';
 
 export default {
-    components: { FCopyButton },
+    components: { FWindow, FCopyButton, VueQRCodeComponent },
     computed: {
         ...mapGetters(['currentAccount']),
     },
@@ -48,10 +66,6 @@ export default {
             if (keystore) {
                 this.$fWallet.downloadKeystore(keystore);
             }
-        },
-
-        onButtonClick() {
-            alert('not implemented yet');
         },
     },
 };
