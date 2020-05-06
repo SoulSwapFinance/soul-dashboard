cb4de91b8159bc9ef26543d3fd9cb3af97f02b2b
Pavel Spacil <pavel.spacil@gmail.com>
2020-05-06 11:50:33 +0200

    Create account history view

---
+++ src/views/AccountHistory.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="view-account-history account-main-content-mt">
+        <account-transaction-list :address="currentAccount.address" />
+    </div>
+</template>
+
+<script>
+import AccountTransactionList from '../components/data-tables/AccountTransactionList.vue';
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'AccountHistory',
+
+    components: { AccountTransactionList },
+
+    computed: {
+        ...mapGetters(['currentAccount']),
+    },
+};
+</script>
