2b91cce1b28da52f8250a74b4b4b7ad6b9ed24f1
Pavel Spacil <pavel.spacil@gmail.com>
2020-06-05 10:49:43 +0200

    Correct scrolling problems

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -4,11 +4,16 @@ import { routes } from '../routes.js';
 
 Vue.use(VueRouter);
 
+/** Don't scroll page to top while navigating to these components. */
+const DONT_SCROLL_TO_TOP = ['account-stake', 'account-send', 'account-receive', 'account-history'];
+
 export const router = new VueRouter({
     routes,
     scrollBehavior(_to, _from, _savedPosition) {
         if (_savedPosition) {
             return _savedPosition;
+        } else if (DONT_SCROLL_TO_TOP.indexOf(_to.name) > -1 && DONT_SCROLL_TO_TOP.indexOf(_from.name) > -1) {
+            return {};
         } else {
             return { x: 0, y: 0 };
         }
