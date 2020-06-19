d81011a9d73a674d466ff0825a465fcf0a5be845
Pavel Spacil <pavel.spacil@gmail.com>
2020-06-19 10:43:18 +0200

    Create Contacts data store and manipulation

---
+++ src/store/store.types.js
@@ -0,0 +1,21 @@
+/**
+ * Account object.
+ * @typedef {Object} WalletAccount
+ * @property {string} address
+ * @property {string} name Custom name of the wallet.
+ * @property {string} balance Hex number.
+ * @property {string} totalBalance Hex number.
+ * @property {string} pendingRewards Hex number.
+ * @property {Object} keystore Keystore file.
+ * @property {boolean} isLedgerAccount
+ * @property {number} accountId Ledger account id.
+ * @property {number} addressId Ledger address id.
+ */
+
+/**
+ * Contact object.
+ * @typedef {Object} WalletContact
+ * @property {string} address
+ * @property {string} name
+ * @property {('fantom' | 'ethereum' | 'binance')} blockchain
+ */
