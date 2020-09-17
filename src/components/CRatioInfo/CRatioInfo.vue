4a2d6defc4a3d59b6efa9e82aaae1f6e4a3ff9cd
Pavel Spacil <pavel.spacil@gmail.com>
2020-09-17 10:37:44 +0200

    Change CRatioInfo text

--- src/components/CRatioInfo/CRatioInfo.vue
+++ src/components/CRatioInfo/CRatioInfo.vue
@@ -1,7 +1,7 @@
 <template>
     <f-info window-closeable window-class="light" window-style="max-width: 500px;" class="debt-limit-f-info">
         C-Ratio needs to be at least 300%.<br />
-        If your C-Ratio is above 500%, you’ll earn 6% APY in wFTM.<br />
+        If your C-Ratio is above 500%, you’ll earn rewards in wFTM.<br />
         If your C-Ratio falls below 300%, your wFTM will be locked (not liquidated) until the C-Ratio goes back to at
         least 300%. That can happen either via FTM price fluctuations or if you add more wFTM as collateral.
     </f-info>
