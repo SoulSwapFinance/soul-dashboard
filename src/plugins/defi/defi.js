505d8cbc992e38e2545aee29f0774d7abe182664
Pavel Spacil <pavel.spacil@gmail.com>
2020-11-10 14:05:45 +0100

    Get rid of `Appolo` in `fetchTokens` query which causing problems
    (cherry picked from commit 5e42baff914852ff9b35d7a85460f8ddfa3dbfe2)

--- src/plugins/defi/defi.js
+++ src/plugins/defi/defi.js
@@ -2,6 +2,7 @@ import './defi.types.js';
 import gql from 'graphql-tag';
 import { isObjectEmpty, lowercaseFirstChar } from '../../utils';
 import web3utils from 'web3-utils';
+import { fFetch } from '@/plugins/ffetch.js';
 
 /** @type {BNBridgeExchange} */
 export let defi = null;
@@ -695,7 +696,7 @@ export class DeFi {
      * @return {Promise<DefiToken[]>}
      */
     async fetchTokens(_ownerAddress, _symbol) {
-        const data = await this.apolloClient.query({
+        const query = {
             query: _ownerAddress
                 ? gql`
                       query DefiTokens($owner: Address!) {
@@ -742,8 +743,11 @@ export class DeFi {
             variables: {
                 owner: _ownerAddress,
             },
-            fetchPolicy: 'network-only',
-        });
+            // fetchPolicy: 'network-only',
+        };
+        // const data = await this.apolloClient.query(query);
+        const data = await fFetch.fetchGQLQuery(query, 'defiTokens');
+
         let defiTokens = data.data.defiTokens || [];
 
         if (filterTokens.length > 0) {
