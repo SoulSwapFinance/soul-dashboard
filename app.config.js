859810098bd226d8bba670d16cb304157e614644
Pavel Spacil <pavel.spacil@gmail.com>
2020-11-27 12:55:45 +0100

    Fix crypto symbol size

--- app.config.js
+++ app.config.js
@@ -58,7 +58,7 @@ const appConfig = {
     // tmp
     disableSFTM: true,
     //
-    useTestnet: true,
+    useTestnet: false,
     // testnet config
     testnet: {
         // list of providers. if one of them is unavailable, another is randomly picked
