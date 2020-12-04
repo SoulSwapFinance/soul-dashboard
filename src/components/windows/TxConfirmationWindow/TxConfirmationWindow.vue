8a409a37d55643f52d6e5c7f6bcfc0ee23324498
Pavel Spacil <pavel.spacil@gmail.com>
2020-12-04 09:02:50 +0100

    Remove components change handler from `TxConfirmationWindow`

--- src/components/windows/TxConfirmationWindow/TxConfirmationWindow.vue
+++ src/components/windows/TxConfirmationWindow/TxConfirmationWindow.vue
@@ -14,24 +14,18 @@
                 <h2>Confirmation</h2>
                 <div>steps</div>
             </template>
-            <component
-                :is="currentComponent"
-                v-bind="currentComponentProperties"
-                @change-component="onChangeComponent"
-                @cancel-button-click="onCancelButtonClick"
-            ></component>
+            <slot></slot>
         </f-window>
     </div>
 </template>
 
 <script>
 import FWindow from '@/components/core/FWindow/FWindow.vue';
-import DefiDepositConfirmation from '@/components/DefiDepositConfirmation/DefiDepositConfirmation.vue';
 
 export default {
     name: 'TxConfirmationWindow',
 
-    components: { DefiDepositConfirmation, FWindow },
+    components: { FWindow },
 
     data() {
         return {
@@ -39,44 +33,12 @@ export default {
         };
     },
 
-    computed: {
-        currentComponentProperties() {
-            if (this.currentComponent) {
-                return this._data_;
-            }
-
-            return null;
-        },
-    },
-
-    created() {
-        // temporary data
-        this._data_ = null;
-    },
-
     methods: {
-        show(_compName, _data = {}) {
-            if (_compName) {
-                this.currentComponent = _compName;
-                this._data_ = _data;
-                this.$refs.win.show();
-            }
+        show() {
+            this.$refs.win.show();
         },
 
-        /**
-         * @param {Object} _data
-         */
-        onChangeComponent(_data) {
-            this._data_ = _data.data;
-
-            this.currentComponent = _data.to;
-
-            this.$nextTick(() => {
-                this._data_ = null;
-            });
-        },
-
-        onCancelButtonClick() {
+        hide() {
             this.$refs.win.hide();
         },
     },
