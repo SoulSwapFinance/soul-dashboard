a9610adf60819f138e163704cdecefa5150032b7
Pavel Spacil <pavel.spacil@gmail.com>
2021-02-11 10:42:02 +0100

    Create skeleton of FLendMarketList component

---
+++ src/components/data-tables/FLendMarketList/FLendMarketList.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="flendmarketlist">
+        <f-data-table
+            :columns="columns"
+            :items="items"
+            :loading="loading"
+            first-m-v-column-width="6"
+            f-card-off
+            action-on-row
+            class="f-data-table-body-bg-color"
+            @row-action="onRowAction"
+        >
+            <template v-slot:column-asset="{ value, item, column }">
+                <div v-if="column" class="row no-collapse no-vert-col-padding">
+                    <div class="col-6 f-row-label">{{ column.label }}</div>
+                    <div class="col break-word">
+                        <f-crypto-symbol :token="item.erc20Info" />
+                    </div>
+                </div>
+                <template v-else>
+                    <f-crypto-symbol :token="item.erc20Info" />
+                </template>
+            </template>
+        </f-data-table>
+    </div>
+</template>
+
+<script>
+import FDataTable from '@/components/core/FDataTable/FDataTable.vue';
+import FCryptoSymbol from '@/components/core/FCryptoSymbol/FCryptoSymbol.vue';
+import { stringSort } from '@/utils/array-sorting.js';
+// import {formatNumberByLocale} from "@/filters.js";
+// import {MAX_TOKEN_DECIMALS_IN_TABLES} from "@/plugins/fantom-web3-wallet.js";
+
+export default {
+    name: 'FLendMarketList',
+
+    components: { FCryptoSymbol, FDataTable },
+
+    data() {
+        return {
+            items: [],
+            columns: [
+                {
+                    name: 'asset',
+                    label: 'Asset',
+                    sortFunc: (_itemProp, _direction = 'asc') => {
+                        return (_a, _b) => {
+                            const a = _a.erc20Info.symbol;
+                            const b = _b.erc20Info.symbol;
+
+                            return (_direction === 'desc' ? -1 : 1) * stringSort(a, b);
+                        };
+                    },
+                    sortDir: 'desc',
+                    width: '180px',
+                },
+                {
+                    name: 'marketsize',
+                    label: 'Market Size',
+                    formatter: () => {
+                        return '-';
+                    },
+                },
+                {
+                    name: 'borrowed',
+                    label: 'Total Borrowed',
+                    formatter: () => {
+                        return '-';
+                    },
+                },
+                {
+                    name: 'depositapy',
+                    label: 'Deposit APY',
+                    itemProp: 'currentLiquidityRate',
+                    formatter: (_value) => {
+                        return _value;
+                    },
+                },
+                {
+                    name: 'variableapr',
+                    label: 'Variable Borrow APR',
+                    itemProp: 'currentVariableBorrowRate',
+                    formatter: (_value) => {
+                        return _value;
+                    },
+                },
+                {
+                    name: 'stableapr',
+                    label: 'Stable Borrow APR',
+                    itemProp: 'currentStableBorrowRate',
+                    formatter: (_value) => {
+                        return _value;
+                    },
+                },
+            ],
+            loading: true,
+        };
+    },
+
+    created() {
+        this.init();
+    },
+
+    methods: {
+        async init() {
+            const reserves = await this.$flend.fetchReservesWithERC20Info();
+
+            this.loading = false;
+            this.items = reserves;
+        },
+
+        /**
+         * @param {{proposal: GovernanceProposal}} _item
+         */
+        onRowAction(_item) {
+            this.$router.push({
+                name: 'f-lend-reserve-detail',
+                params: {
+                    assetAddress: _item.assetAddress,
+                },
+            });
+        },
+    },
+};
+</script>
