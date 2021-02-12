86093e3f488e3b2ed64d9c37d8c97c21e0fc3e92
Pavel Spacil <pavel.spacil@gmail.com>
2021-02-12 08:26:18 +0100

    Create FYesNo component

---
+++ src/components/core/FYesNo/FYesNo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="fyesno">
+        <span v-if="value" class="fyesno_yes">yes</span>
+        <span v-else class="fyesno_no">no</span>
+    </div>
+</template>
+
+<script>
+/**
+ * Component for displaying text 'yes' in green color or 'no' in red color.
+ */
+export default {
+    name: 'FYesNo',
+
+    props: {
+        value: {
+            type: Boolean,
+            default: false,
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@import 'style';
+</style>
