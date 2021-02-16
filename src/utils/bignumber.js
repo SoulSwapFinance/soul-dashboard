04638a2f96b59a99e6c05749422d65a83d2d3038
Pavel Spacil <pavel.spacil@gmail.com>
2021-02-16 06:25:18 +0100

    bignumber.js: add bFromWei function

--- src/utils/bignumber.js
+++ src/utils/bignumber.js
@@ -1,5 +1,17 @@
 import { BigNumber } from 'bignumber.js';
 
+/**
+ * @param {string|number|BigNumber} _value
+ * @return {BigNumber}
+ */
 export function toBigNumber(_value) {
     return new BigNumber(_value);
+}
+
+/**
+ * @param {string|number|BigNumber} _value
+ * @return {BigNumber}
+ */
+export function bFromWei(_value) {
+    return toBigNumber(_value).shiftedBy(-18);
 }
