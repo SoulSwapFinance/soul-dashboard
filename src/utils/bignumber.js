a65f600ae7a31dbd1812c09bd7675ed5526d60f6
Pavel Spacil <pavel.spacil@gmail.com>
2021-02-17 11:03:14 +0100

    bignumber.js: add methods bShiftDP and bToWei

--- src/utils/bignumber.js
+++ src/utils/bignumber.js
@@ -9,9 +9,32 @@ export function toBigNumber(_value) {
 }
 
 /**
+ * Shift decimal point
+ *
+ * @param {string|number|BigNumber} _value
+ * @param {number} [_dp]
+ * @return {BigNumber}
+ */
+export function bShiftDP(_value, _dp = 18) {
+    return toBigNumber(_value).shiftedBy(_dp);
+}
+
+/**
+ * From Wei to Ether
+ *
  * @param {string|number|BigNumber} _value
  * @return {BigNumber}
  */
 export function bFromWei(_value) {
-    return toBigNumber(_value).shiftedBy(-18);
+    return bShiftDP(_value, -18);
+}
+
+/**
+ * From Ether to Wei
+ *
+ * @param {string|number|BigNumber} _value
+ * @return {BigNumber}
+ */
+export function bToWei(_value) {
+    return bShiftDP(_value, 18);
 }
