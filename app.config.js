748975e5874ae65d9507a55bc7b21eb3ac83cc97
Pavel Spacil <pavel.spacil@gmail.com>
2021-03-02 06:22:23 +0100

    Load price chart data when resolution is changed
    Switch to mainnet

--- app.config.js
+++ app.config.js
@@ -75,7 +75,7 @@ const appConfig = {
     },
     disableFLend: true,
     //
-    useTestnet: true,
+    useTestnet: false,
     // testnet config
     testnet: {
         // list of providers. if one of them is unavailable, another is randomly picked
