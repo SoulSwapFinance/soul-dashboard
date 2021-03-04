61f55ea56faa7b29b85ce764b0f841c658cf4320
Pavel Spacil <pavel.spacil@gmail.com>
2021-03-04 10:19:36 +0100

    Settings: add Metamask section

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -2,17 +2,22 @@
     <div class="view-settings">
         <h1>Settings</h1>
 
+        <h2>fWallet</h2>
         <settings-form />
+
+        <h2>Metamask</h2>
+        <metamask-settings />
     </div>
 </template>
 
 <script>
-import SettingsForm from '../../components/forms/SettingsForm.vue';
+import SettingsForm from '@/components/forms/SettingsForm.vue';
+import MetamaskSettings from '@/components/metamask/MetamaskSettings/MetamaskSettings.vue';
 
 export default {
     name: 'Settings',
 
-    components: { SettingsForm },
+    components: { MetamaskSettings, SettingsForm },
 };
 </script>
 
