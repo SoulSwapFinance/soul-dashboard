c0dfb7023c442bf7fc1fc6bce07259f1b383f76a
Pavel Spacil <pavel.spacil@gmail.com>
2021-03-05 05:35:12 +0100

    Create SettingsLinks component

---
+++ src/views/Settings/SettingsLinks.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="settingslinks">
+        <f-card class="f-card-double-padding">
+            <div class="small-container">
+                <p>
+                    <a href="https://fantom.foundation/how-to-use-fantom-wallet/" target="_blank">
+                        How to use Fantom fWallet
+                    </a>
+                </p>
+
+                <p>
+                    <a href="https://explorer.fantom.network/" target="_blank">
+                        Fantom Explorer
+                    </a>
+                </p>
+
+                <social-media-links />
+            </div>
+        </f-card>
+    </div>
+</template>
+
+<script>
+import SocialMediaLinks from '@/components/SocialMediaLinks/SocialMediaLinks.vue';
+import FCard from '@/components/core/FCard/FCard.vue';
+
+export default {
+    name: 'SettingsLinks',
+
+    components: { FCard, SocialMediaLinks },
+};
+</script>
