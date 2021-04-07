dfd2e25280a710fd11fe2783f44b18b161af4a07
Pavel Spacil <pavel.spacil@gmail.com>
2021-04-07 06:06:57 +0200

    Remove ballots

--- src/views/GovHome/GovHome.vue
+++ src/views/GovHome/GovHome.vue
@@ -3,19 +3,15 @@
         <h1>Governance</h1>
 
         <gov-proposal-list />
-
-        <br />
-        <ballot-list />
     </div>
 </template>
 
 <script>
 import GovProposalList from '@/components/data-tables/GovProposalList/GovProposalList.vue';
-import BallotList from '@/components/data-tables/BallotList/BallotList.vue';
 
 export default {
     name: 'GovHome',
 
-    components: { BallotList, GovProposalList },
+    components: { GovProposalList },
 };
 </script>
